ef489d6efb313273aabbc981e63643696f8518ce
Megan Hoffman <meganhoffman@megans-mbp.lan>
2018-11-17 07:43:32 -0800

    Initiated Jasmine and test suite, TODO

--- primes-multiplication-table.js
+++ primes-multiplication-table.js
@@ -1,1 +1,10 @@
-console.log('Hello, world');
+// A program that prints out a multiplication table of the first 10 prime numbers.
+
+/**
+ * TODO: 
+ * Find first 10 (or n) primes (use sieve of Eratosthenes)
+ * Store primes for reference
+ * Multiply the primes, only amount of times necessary to display table
+ * Display table
+ */
+
