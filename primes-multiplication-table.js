fc109abd1cafc22ee839e35be00881d804944658
Megan Hoffman <meganhoffman@megans-mbp.lan>
2018-11-17 07:55:00 -0800

    Set initial fcn declarations

--- primes-multiplication-table.js
+++ primes-multiplication-table.js
@@ -8,3 +8,27 @@
  * Display table
  */
 
+// Store first n primes in some data structure, use another function to determine if prime
+function storePrimes() {
+    /**
+     * TESTS
+     * n = 10, are all of the primes that are supposed to be stored here
+     * primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+     */
+
+}
+
+// Determine if a number is prime
+function isPrime() {
+
+}
+
+// Complete necessary multiplication of primes to fill table
+function multiplyPrimes() {
+
+}
+
+// Display table with the values from multiplyPrimes()
+function displayTable () {
+
+}
