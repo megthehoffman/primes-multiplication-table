c390df57b5bb5601024589590a283ab35fc0a988
Megan Hoffman <meganhoffman@megans-mbp.lan>
2018-11-17 21:03:07 -0800

    Added tests for multiplyPrimes

--- spec/primes-multiplication-table-spec.js
+++ spec/primes-multiplication-table-spec.js
@@ -26,5 +26,15 @@ describe("Primes multiplcation table", function() {
   });
   });
 
+  describe("multiplyPrimes", function() {
+  it("generates an array of arrays, corresponding to table rows", function() {
+    expect(primes.multiplyPrimes([])).toEqual([]);
+    expect(primes.multiplyPrimes([2])).toEqual([['', 2], [2, 4]]);
+    expect(primes.multiplyPrimes([2, 3])).toEqual([['', 2, 3], [2, 4, 6], [3, 6, 9]]);
+    expect(primes.multiplyPrimes([2, 3, 5, 7])).toEqual([['', 2, 3, 5, 7], [2, 4, 6, 10, 14], [3, 6, 9, 15, 21], 
+                                                      [5, 10, 15, 25, 35], [7, 14, 21, 35, 49]]);
+    expect(primes.multiplyPrimes([2, 3, 5, 7, 11, 13, 17, 19, 23, 29])[11][11]).toEqual(841);
+  });
+  });
 });
 
